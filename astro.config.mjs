1f9d85a38edde925b8fb5e3cabbf31869b9ae0e8
juliopelusa <julioalvarez1950@hotmail.es>
2026-06-10 19:02:25 -0300

    agregamos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-import alpinejs from '@astrojs/alpinejs';
-
-// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
-  integrations: [alpinejs()]
-});
+});  
